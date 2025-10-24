daf2b14af14f1c281077d43a8be5df06f7d5f0df
alexgrimaldi <alexgrimaldi@gmail.com>
2025-10-24 11:19:12 +0100

    remove it

--- tapo_device.py
+++ tapo_device.py
@@ -1,4 +1,4 @@
 from tapo import ApiClient
 def get_client():
-    client = ApiClient(tapo_username="alexgrimaldi@gmail.com", tapo_password="k0b3BRYANT")
+    client = ApiClient(tapo_username="alexgrimaldi@gmail.com", tapo_password="**********")
     return client
